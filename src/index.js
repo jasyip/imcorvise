13e8df28abf02efa8187359dcf738c63191eb6d2
Jason <jasonyip1234567@gmail.com>
2020-12-19 21:10:45 -0800

    [WIP]: Experimentation with OSMD

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-(function ()
-    {
-        var canvas;
-        var openSheetMusicDisplay;
+import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
 
-        function init()
+
+
+(function ()
+{
+
+    function startOSMD(event)
+    {
+        let file = event.target.files[0];
+        if (file)
         {
+            let canvas;
+            let openSheetMusicDisplay;
+
             canvas = document.createElement("div");
             openSheetMusicDisplay = new OpenSheetMusicDisplay(canvas);
             openSheetMusicDisplay.setLogLevel("info");
             document.body.appendChild(canvas);
-        }
 
-    }());
+            openSheetMusicDisplay.load(DocumentBuilderFactory.newInstance();
+            openSheetMusicDisplay.render();
+        }
+    }
+
+    function init()
+    {
+        const input = document.querySelector("#input");
+        input.addEventListener("change", startOSMD);
+    }
+
+    window.addEventListener("load", function(){
+        init();
+    });
+
+}());
