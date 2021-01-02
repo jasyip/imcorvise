e56ec1af3652039f3fc88325882ef4fc812b7f37
Jason <jasonyip1234567@gmail.com>
2021-01-01 16:12:37 -0800

    [ADD]: Directions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { ImcorviseGraphicalMusicSheet } from "./Graphical/ImcorviseGraphicalMusi
 {
     let imcorvise;
     let imcorviseDiv;
+    let submitButton;
+    let directions;
 
     function submit(event)
     {
@@ -15,6 +17,9 @@ import { ImcorviseGraphicalMusicSheet } from "./Graphical/ImcorviseGraphicalMusi
             alert("Select some measures first!");
             return;
         }
+        submit.disabled = true;
+        directions.innerHTML =
+            "Your file is being processed to cater your selection of measures."
         imcorvise.GraphicSheet.disableInteraction();
     }
 
@@ -22,7 +27,10 @@ import { ImcorviseGraphicalMusicSheet } from "./Graphical/ImcorviseGraphicalMusi
     {
         imcorvise.load(text);
         imcorvise.render();
-        const submitButton = document.getElementById("submit");
+        directions.innerHTML =
+            "Select all mostly-rest measures part of a solo."
+            + "<br>Hold down left-click and drag to select measures. "
+            + "Hold down right-click and drag to unselect measures.";
         submitButton.hidden = false;
         submitButton.addEventListener("click", submit);
     }
@@ -36,7 +44,8 @@ import { ImcorviseGraphicalMusicSheet } from "./Graphical/ImcorviseGraphicalMusi
             imcorvise = new Imcorvise(imcorviseDiv);
             imcorvise.setLogLevel("info");
             document.body.appendChild(imcorviseDiv);
-
+            directions.innerHTML = "Your MusicXML file is being read at the moment. "
+                + "Time needed to process is proportional to the file's size. ";
             file.text().then(startImcorvise);
         }
     }
@@ -44,12 +53,12 @@ import { ImcorviseGraphicalMusicSheet } from "./Graphical/ImcorviseGraphicalMusi
     function init()
     {
         const input = document.getElementById("input");
+        submitButton = document.getElementById("submit");
+        directions = document.getElementById("directions");
+        directions.innerHTML = "Upload a MusicXML file to get started!"
         input.addEventListener("change", loadFile);
     }
 
-    window.addEventListener("load", function()
-    {
-        init();
-    });
+    window.addEventListener("load", init); 
 
 }());
